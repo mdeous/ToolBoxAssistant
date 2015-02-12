0881df2e475aace9512a1cc426763a3ad514b7d6
Mathieu Deous <mat.deous@gmail.com>
2015-02-12 11:47:11 +0100

    create setup script

---
+++ setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import os.path
+from setuptools import setup, find_packages
+
+from ToolBoxAssistant import VERSION
+
+CURRENT_DIR = os.path.realpath(os.path.dirname(__file__))
+
+
+setup(
+    name='ToolBoxAssistant',
+    version=VERSION,
+    description='An utility to easily manage your toolbox applications',
+    long_description=open(os.path.join(CURRENT_DIR, 'README.md')).read(),
+    author='Mathieu D. (MatToufoutu)',
+    author_email='mattoufootu+code@gmail.com',
+    url='https://github.com/mattoufoutu/ToolBoxAssistant',
+    license='GPL',
+    keywords='toolbox tools applications management utility',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    scripts=[os.path.join(CURRENT_DIR, 'ToolBoxAssistant', 'bin', 'tba')]
+)
