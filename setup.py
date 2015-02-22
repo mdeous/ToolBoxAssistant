46dd61b38c317918d7d0c187c8ad66d2a7c8e08b
Mathieu Deous <mat.deous@gmail.com>
2015-02-22 23:23:44 +0100

    use relative path for script path in setup (compliance with latest setuptools versions)

--- setup.py
+++ setup.py
@@ -30,5 +30,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=True,
-    scripts=[os.path.join(CURRENT_DIR, 'ToolBoxAssistant', 'bin', 'tba')]
+    scripts=[os.path.join('.', 'ToolBoxAssistant', 'bin', 'tba')]
 )
